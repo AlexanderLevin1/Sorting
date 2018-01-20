58164f027154dd61d4631a3ece2a7aaad3ffe607
Alexander Levin <alexanderjlevin1@gmail.com>
2018-01-20 11:05:26 -0500

    added notes

--- bubblesort.js
+++ bubblesort.js
@@ -20,3 +20,19 @@ function bubbleSort(arr) {
     }
     return arr;
 }
+
+/*
+function bubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for(var j = 0; j < arr.length; j++) {
+            if(typeof arr[j] !== 'undefined' && typeof arr[j + 1] !== 'undefined') {
+                if(arr[j] > arr[j + 1]) {
+                    var temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
--- mergesort.js
+++ mergesort.js
@@ -20,7 +20,7 @@ function merge(left, right) {
             result.push(right[iR++])
         }
     }
-return result.concat(left.slice(iL)).concat(right.slice(iR))
+return result.concat(left.slice(iL)).concat(right.slice(iR));
 }
 
 /* Concise merge syntax
@@ -40,4 +40,46 @@ function mergeSort(arr) {
   return merge(mergeSort(split(arr)[0]), mergeSort(split(arr)[1]));
 }
 
+
 console.log(mergeSort(a));
+/* Notes:
+-- an array of length 1 is sorted
+-- can't return two variables, have to return an object (or array)
+*/
+
+/*
+function mergeSort(arr) {
+    if (arr.length <= 2) {
+        return arr;
+    }
+    var middle - Math.floor(arr.length / 2);
+    var left = mergeSort(arr.slice(0, middle));
+    var right = mergeSort(arr.slice(middle));
+
+    var results = [];
+    while (left.length && right.length) {
+        if(left[0] < right[0]) { 
+            results.push(left.shift())
+        }
+        else { 
+            results.push(right.shift())
+        }
+    }
+if (left.length) {
+    results = results.concat(left);
+} 
+if (right.length) {
+    results = results.concat(right);
+}
+return results;
+}
+
+console.log(mergeSort([2, 1, 0]);
+[2], [ 0, 1]
+- result [0]
+left with [2] and [1] and compares those
+- result [0 , 1];
+left with [2] and [];
+[0, 1, 2];
+
+*/
